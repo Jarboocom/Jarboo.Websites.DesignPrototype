749e9a71c5c53e710ca197f0462f493a48d1b229
Lars Holdgaard <mcoroklo@gmail.com>
2015-08-30 19:58:11 +0200

    some initial commits

---
+++ src/Website/Domain/JarbooPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Domain
+{
+    public class JarbooPage
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string DateCreated { get; set; }
+
+        public string SeoTitle { get; set; }
+        public string SeoMetaDescription { get; set; }
+    }
+}
---
+++ src/Website/Services/ContentService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Website.Domain;
+
+namespace Website.Services
+{
+    public class ContentService
+    {
+        private const string DomainBase = "http://blog.jarboo.com/";
+        private const string ApiBase = "api/get_page_index?slug=";
+
+        public ContentService()
+        {
+
+        }
+
+        public JarbooPage GetBySlug(string slug)
+        {
+            var url = string.Format("{0}{1}", DomainBase, ApiBase);
+
+            using (var client = new WebClient())
+            {
+                var data = client.DownloadString(url);
+
+
+            }
+        }
+
+    }
+}
