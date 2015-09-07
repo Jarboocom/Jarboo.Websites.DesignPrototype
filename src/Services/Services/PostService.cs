ce098825b8c1e61bffaabb17be3bcd9b7e953e6d
Vsevolod Goloviznin <vsevolod.goloviznin@gmail.com>
2015-09-07 19:14:46 +0300

    Added PageService to fetch data

--- src/Services/Services/PostService.cs
+++ src/Services/Services/PostService.cs
@@ -6,34 +6,29 @@ using Services.Domain.Pages.DTO;
 
 namespace Services.Services
 {
-    public class ContentService:IContentService
+    public class PostService : IPostService
     {
         private const string DomainBase = "http://blog.jarboo.com/";
-        private const string ApiBase = "api/get_page?slug=";
+        private const string ApiBase = "api/get_post?slug=";
 
-        public ContentService()
-        {
-
-        }
 
         public JarbooPage GetBySlug(string slug)
         {
-            var url = string.Format("{0}{1}{2}", DomainBase, ApiBase,slug);
+            var url = string.Format("{0}{1}{2}", DomainBase, ApiBase, slug);
 
-            using (var client = new WebClient())
+            try
             {
-                try
+                using (var client = new WebClient())
                 {
                     var data = client.DownloadString(url);
 
-                    var page = JsonConvert.DeserializeObject<WordpressPage>(data);
+                    var page = JsonConvert.DeserializeObject<WordpressPost>(data);
                     return ConvertDTOToObject(page);
                 }
-                catch (Exception)
-                {
-                    return null;
-                }
-                
+            }
+            catch (Exception)
+            {
+                return null;
             }
         }
 
@@ -52,14 +47,14 @@ namespace Services.Services
             throw new NotImplementedException();
         }
 
-        private JarbooPage ConvertDTOToObject(WordpressPage page)
+        private JarbooPage ConvertDTOToObject(WordpressPost page)
         {
             return new JarbooPage
             {
-                Content = page.page.content,
-                DateCreated = page.page.date,
-                Title = page.page.title,
-                Slug = page.page.slug
+                Content = page.post.content,
+                DateCreated = page.post.date,
+                Title = page.post.title,
+                Slug = page.post.slug
             };
         }
     }
